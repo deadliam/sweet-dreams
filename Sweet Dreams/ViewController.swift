04a5d5492a56fd43c47c103488c9bad2e1069f9c
Anatolii Kasianov <tommy@macpaw.com>
2022-08-29 20:49:23 +0300

    Initial Commit

---
+++ Sweet Dreams/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Sweet Dreams
+//
+//  Created by Anatolii Kasianov on 29.08.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
